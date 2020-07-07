3ff03622e24313016fd22bc87c119999edcb2715
Senthil <spk2dc@virginia.edu>
2020-07-07 14:31:45 -0400

    adding environment variables in

--- controllers/router.js
+++ controllers/router.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+require('dotenv').config()
+
+const CLIENT_ID = process.env.CLIENT_ID
 
 router.get('/', (req, res) => {
-    res.send('Hello from router!');
+    res.send(`Testing environment variable ${CLIENT_ID}`);
 });
 
 module.exports = router;
